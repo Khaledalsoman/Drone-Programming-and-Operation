4c4011a8568b99fdcd377e719a64bcfade0a4485
khaledalsoman <khaledalsoman@gmail.com>
2026-02-08 19:49:43 +0300

    Mavsdk code complete

--- MAVsdk_Hello.py
+++ MAVsdk_Hello.py
@@ -5,4 +5,7 @@ async def run():
     drone=System()
     print("connecting to px4........")
 
-    await drone.connect(System_addres='')
+    await drone.connect(System_addres="udp://:14540")
+    print('drone connected')
+
+    asyncio.run(run())
