4ab4556c5097ab763ef2ea7b27f87fc5cc5f340d
khaledalsoman <khaledalsoman@gmail.com>
2026-02-09 19:02:07 +0300

    udpating mavsdk hello

--- MAVsdk_Hello.py
+++ MAVsdk_Hello.py
@@ -4,8 +4,25 @@ from mavsdk import System
 async def run():
     drone=System()
     print("connecting to px4........")
+    await drone.connect(system_address="udpin://0.0.0.0:14540")
+    print('waiting for the drone to connect')
 
-    await drone.connect(System_addres="udp://:14540")
-    print('drone connected')
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"drone connected")
+            break
+    print("arming...")
+    await drone.action.arm()
+    print("aremed..")
 
-    asyncio.run(run())
+    
+    print("taking off...")
+    await drone.action.takeoff()
+    await asyncio.sleep(10)
+
+    print('landing...')
+    await drone.action.land()
+    print('drone landed!!!')
+asyncio.run(run())
+
+
